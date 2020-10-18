aaa494a065b87e609d13e0a08c9f195f5c968cc3
Dynee <lucaspadden@gmail.com>
2020-10-18 19:49:11 -0400

    removed folders, refactoring from fresh restart, I have a better idea on how to actually design/build this now

--- src/features/button/PrimaryButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-
-export const PrimaryButton = ({ onClick, children }) => (
-    <Button fullWidth onClick={onClick} variant="contained" color="primary">
-      {children}
-    </Button>
-)
